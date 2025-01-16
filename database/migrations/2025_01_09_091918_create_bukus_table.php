632817fa24cbec678068dc0cacaefa5a99b7ee92
ferdi155 <alfnsh155@gmail.com>
2025-01-16 16:03:32 +0700

    schema database clear

--- database/migrations/2025_01_09_091918_create_bukus_table.php
+++ database/migrations/2025_01_09_091918_create_bukus_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('bukus', function (Blueprint $table) {
             $table->id();
             $table->foreignId('kategori_id')->constrained('kategori_bukus');
+            $table->string('kode_buku');
             $table->string('judul');
             $table->string('penulis');
             $table->string('penerbit');
